3a558e0b783dd791def34b58079894eab0b20fdb
iooiQllQ <oiiogg4@gmail.com>
2020-07-09 16:56:36 +0800

    first commit

---
+++ src/main/java/JpaApplication.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(com.springBoot.jpa.JpaApplication.class, args);
+	}
+
+}
+
